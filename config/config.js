a394b37c29db6978f003a48b9f5409ad212267c5
LarsVerschoor <larsverschoor2@gmail.com>
2024-12-10 14:51:24 +0100

    removed query data logging in terminal

--- config/config.js
+++ config/config.js
@@ -5,13 +5,7 @@ module.exports = {
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
-		dialect: process.env.DB_DIALECT,
-		logging: (query, queryObject) => {
-			console.log('SQL Query:', query);
-			if (queryObject && queryObject.bind) {
-				console.log('Bound Values:', queryObject.bind);
-			}
-		}
+		dialect: process.env.DB_DIALECT
 	},
 	test: {
 		username: process.env.DB_USER,
